c843733be7fd8aeb8a0678711f0966cfc049f023
Nicholas Balderas <geist732@gmail.com>
2021-10-09 07:54:22 -0500

    Tried starting unit tests. Might need more time to study up on best practices, etc. Will come back to this.

---
+++ Assets/Tests/EditMode/GeneratorTests.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+public class GeneratorTests
+{
+    
+    // [Test]
+    // public void Pickup()
+    // {
+    //     GameObject item = GameObject.CreatePrimitive(PrimitiveType.Cube);
+    //     
+    // }
+}
---
+++ Assets/Tests/PlayMode/GeneratorTests.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class GeneratorTests
+{
+    
+    // private static Collider _playerCollider = GameObject.Find("Player").GetComponent<Collider>();
+
+    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+    // `yield return null;` to skip a frame.
+    // [UnityTest] 
+    // public IEnumerator OnTriggerEnter([ValueSource(nameof(_playerCollider))] Collider other)
+    // {
+    //     yield return null;
+    //     Assert.IsTrue(other.CompareTag("Player"));
+    //     
+    // }
+}
