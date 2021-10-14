80c1a6414af1f9241ef1efedae5c48fa0fd41fc8
Nicholas Balderas <geist732@gmail.com>
2021-10-14 11:26:33 -0500

    Established OrderQueue and added methods in game manager to add/remove/read orderQueue List.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,4 +2,36 @@ using UnityEngine;
 
 public class  GameManager : MonoBehaviour
 {
+    private OrderQueue _orderQueue;
+    private void Awake()
+    {
+        _orderQueue = new OrderQueue();
+    }
+
+    private void Start()
+    {
+        // TODO: Remove hard-coded string for easy mode. Enum?
+        // _orderQueue.GenerateRandomOrder("EasyRecipes.json");
+    }
+    
+    [ContextMenu("Add Order To Queue")]
+    void AddOrder()
+    {
+        _orderQueue.GenerateRandomOrder("EasyRecipes.json");
+    }
+
+    [ContextMenu("Remove Order From Queue")]
+    void RemoveOrder()
+    {
+        _orderQueue.RemoveOrderFromQueue(_orderQueue.orderQueue[Random.Range(0, _orderQueue.orderQueue.Count)]);
+    }
+
+    [ContextMenu("Read Orders")]
+    void ReadOrders()
+    {
+        foreach (var order in _orderQueue.orderQueue)
+        {
+            Debug.Log(order.name);
+        }
+    }
 }
---
+++ Assets/Scripts/OrderQueue.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Model;
+using UnityEngine;
+using Utility;
+
+public class OrderQueue
+{
+    public List<Recipe> orderQueue = new List<Recipe>();
+
+    public void AddOrderToQueue(Recipe recipe)
+    {
+        orderQueue.Add(recipe);
+    }
+
+    public bool RemoveOrderFromQueue(Recipe recipe)
+    {
+        return orderQueue.Remove(recipe);
+    }
+
+    public void GenerateRandomOrder(string difficulty)
+    {
+        string path = Application.dataPath + "/Data/" + difficulty;
+        string contents = File.ReadAllText(path);
+        List<Recipe> existingRecipes = JsonHelper.FromJson<Recipe>(contents).ToList();
+        var randomRecipe = existingRecipes[Random.Range(0, existingRecipes.Count)];
+        AddOrderToQueue(randomRecipe);
+    }
+}
